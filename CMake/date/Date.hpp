ebc57825eb1a71892ff6e3e0a59ef92563f8f50d
Marko Raatikainen <marko.m.raatikainen@gmail.com>
2014-04-06 22:01:55 +0300

    Export the dll function only on windows

--- CMake/date/Date.hpp
+++ CMake/date/Date.hpp
@@ -1,10 +1,12 @@
 #ifndef TUTORIAL_DATE_HPP_INCLUDED
 #define TUTORIAL_DATE_HPP_INCLUDED
 
+#ifdef _WIN32
 #ifdef DATE_LIBRARY_BUILD
 #define DATE_LIBRARY_EXPORT __declspec(dllexport)
 #else
 #define DATE_LIBRARY_EXPORT __declspec(dllimport)
+#endif
 #endif
 
 #include <string>
