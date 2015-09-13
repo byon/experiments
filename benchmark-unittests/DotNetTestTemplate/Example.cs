508f7b6e27b34b21450e1ec4a4dae83e89993002
Marko Raatikainen <marko.m.raatikainen@gmail.com>
2015-09-13 17:42:50 +0300

    Example DotNet test

---
+++ benchmark-unittests/DotNetTestTemplate/Example.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DotNetTestTemplate
+{
+    [TestClass]
+    public class Example
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
